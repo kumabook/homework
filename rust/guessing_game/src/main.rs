6c6bf4d5f58316799ab4e89b54c2ed04fb513a2b
Hiroki Kumamoto <kumabook@live.jp>
2015-09-01 20:53:22 +0900

    Update rust guessing game (1.2.0)

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,45 +1,39 @@
-use std::old_io;
-use std::rand;
+extern crate rand;
+
+use std::io;
 use std::cmp::Ordering;
+use rand::Rng;
+
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = (rand::random::<u32>() % 100 + 1); // secret_nubmer: i32
-
-    println!("The secret number is: {}", secret_number);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     loop {
         println!("Please input your guess.");
 
-        let input = old_io::stdin().read_line()
-                                   .ok()
-                                   .expect("Failed to read line");
-        let input_num: Result<u32, _> = input.trim().parse();
+        let mut guess = String::new();
 
-        let num = match input_num {
-            Ok(n) => n,
-            Err(_) => {
-                println!("Please input a number!");
-                continue;
-            }
+        io::stdin().read_line(&mut guess)
+            .ok()
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_)  => continue,
         };
 
-        println!("You guessed: {}", num);
+        println!("You guessed: {}", guess);
 
-        match cmp(num, secret_number) {
+        match guess.cmp(&secret_number) {
             Ordering::Less    => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal   => {
                 println!("You win!");
-                return;
-            },
+                break;
+            }
         }
     }
 }
 
-fn cmp(a: u32, b: u32) -> Ordering {
-    if a < b      { Ordering::Less }
-    else if a > b { Ordering::Greater }
-    else          { Ordering::Equal }
-}
